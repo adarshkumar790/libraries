2376804408afa862d86bb50440412904b7ecf6f6
adarshkumar790 <126936165+adarshkumar790@users.noreply.github.com>
2024-08-13 15:02:20 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const Connection = async () => {
     try{
-    mongoose.connect("mongodb+srv://AdarshKumar:7903848803@cluster0.bpglqqv.mongodb.net/")
+    mongoose.connect("mongodb+srv://kumaradarsh9379:<password>@cluster0.ipzi9pa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     console.log("Connected")
     }
     catch(error) {
